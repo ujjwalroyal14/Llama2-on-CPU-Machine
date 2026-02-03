740a7085f9b58fe847c0a8faa57b81d14d33b5ee
Ujjwal kumar <ujjwalroyal14@gmail.com>
2026-02-03 08:29:51 +0530

    updated

--- README.md
+++ README.md
@@ -31,13 +31,3 @@ pip install -r requirements.txt
 
 
 ~~~ini
-## Download the Llama 2 Model:
-
-llama-2-7b-chat.ggmlv3.q4_0.bin
-
-
-
-
-## From the following link:
-https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/tree/main
-~~~
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "generative_ai_project",
+    version= '0.0.0',
+    author= 'Ujjwal',
+    author_email= 'ujjwalroyal14@gmail.com',
+    packages= find_packages(),  #find_packages() will find the constructors file in src folder
+    install_requires = []
+)
