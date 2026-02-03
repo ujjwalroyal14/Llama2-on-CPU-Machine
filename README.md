31ad95d55ed339163d5cd5c7b577942b07b50b17
Ujjwal kumar <ujjwalroyal14@gmail.com>
2026-02-03 23:40:42 +0530

    Updated

--- README.md
+++ README.md
@@ -31,3 +31,13 @@ pip install -r requirements.txt
 
 
 ~~~ini
+## Download the Llama 2 Model:
+
+llama-2-7b-chat.ggmlv3.q4_0.bin
+
+
+
+
+## From the following link:
+https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/tree/main
+~~~
--- src/run_local.py
+++ src/run_local.py
@@ -1,2 +1,24 @@
-def test():
-    print("ok")
+from langchain import PromptTemplate
+from langchain import LLMChain
+from langchain.llms import CTransformers
+from src.helper import *
+
+B_INST, E_INST = "[INST]", "[/INST]"
+B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
+
+instruction="Convert the following text from English to Hindi: \n\n {text}"
+
+SYSTEM_PROMPT = B_SYS + DEFAULT_SYSTEM_PROMPT + E_SYS
+template = B_INST + SYSTEM_PROMPT + instruction + E_INST
+
+prompt = PromptTemplate(template=template, input_variables=["text"])
+
+llm=CTransformers(model="model/llama-2-7b-chat.ggmlv3.q4_0.bin",
+                  model_type="llama",
+                  config = {'max_new_tokens':128,
+                          'temperature':0.01}
+                  )
+
+llm_chain=LLMChain(prompt=prompt, llm=llm)
+
+print(llm_chain.run("How are you?"))
